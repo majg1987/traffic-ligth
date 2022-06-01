1ef184aa9760da9b38064087bdcd0bdfdbe3a8fc
majg1987 <majg1987@hotmail.com>
2022-06-01 15:05:48 +0000

    Opcion de que las luces del semaforo funcionen automaticamente

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Semaforo = () => {
 	const [red, setRed] = useState(true);
 	const [ambar, setAmbar] = useState(false);
 	const [green, setGreen] = useState(false);
+	let time = 2000;
 
 	function cambiarRed() {
 		setRed(true);
@@ -21,8 +22,39 @@ const Semaforo = () => {
 		setGreen(true);
 	}
 
+	function cambiarAutomaticamente() {
+		for (let i = 0; i < 100; i++) {
+			setTimeout(() => {
+				cambiarGreen();
+			}, time);
+			time += 4000;
+			setTimeout(() => {
+				cambiarAmbar();
+			}, time);
+			time += 2000;
+			setTimeout(() => {
+				cambiarRed();
+			}, time);
+			time += 2000;
+		}
+	}
+
 	return (
 		<div className="container-fluid p-0">
+			<div
+				className="btn-group position-absolute top-0 start-50 translate-middle-x mt-2"
+				role="group"
+				aria-label="Basic example">
+				<button
+					type="button"
+					className="btn btn-success"
+					onClick={cambiarAutomaticamente}>
+					Auto
+				</button>
+				<button type="button" className="btn btn-primary">
+					Right
+				</button>
+			</div>
 			<div className="soporteSemaforo bg-black"></div>
 			<div className="semaforo bg-black rounded d-flex flex-column">
 				<div
